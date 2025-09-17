b9ec66ed5e07041bc56643fd483285515ef7e5ee
t2alni00 <t2alni00@students.oamk.fi>
2025-09-17 13:19:25 +0300

    AuthenticationForm, login and sign up buttons

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,32 +1,41 @@
-import {Autocomplete, Box, Button, Group, Text,} from '@mantine/core';
+import {Autocomplete, Box, Button, Group, Text, Modal,} from '@mantine/core';
 import {IconSearch} from "@tabler/icons-react";
 import * as React from "react";
 import classes from './DefaultLayout.module.css';
+import { LoginForm } from '../components/AuthenticationForm';
+import { useDisclosure } from '@mantine/hooks';
 
 const DefaultLayout = ({children}: React.PropsWithChildren) => {
+    const [opened, { open, close }] = useDisclosure(false);
+
     return (
-        <Box pb={120}>
-            <header className={classes.header}>
-                <Group justify="space-between" h="100%">
-                    <Text size="lg" fw={700}>Movie App</Text>
-                    <Group h="100%" gap={10} visibleFrom="sm">
-                        <Autocomplete
-                            className={classes.search}
-                            placeholder="Search for a movie"
-                            leftSection={<IconSearch size={16} stroke={1.5} />}
-                            data={[]}
-                            visibleFrom="xs"
-                        />
-                        <Button variant="gradient">Search</Button>
+        <>
+            <Box pb={120}>
+                <header className={classes.header}>
+                    <Group justify="space-between" h="100%">
+                        <Text size="lg" fw={700}>Movie App</Text>
+                        <Group h="100%" gap={10} visibleFrom="sm">
+                            <Autocomplete
+                                className={classes.search}
+                                placeholder="Search for a movie"
+                                leftSection={<IconSearch size={16} stroke={1.5} />}
+                                data={[]}
+                                visibleFrom="xs"
+                            />
+                            <Button variant="gradient">Search</Button>
+                        </Group>
+                        <Group visibleFrom="sm">
+                            <Button variant="default" onClick={open}>Log in</Button>
+                            <Button onClick={open}>Sign up</Button>
+                        </Group>
                     </Group>
-                    <Group visibleFrom="sm">
-                        <Button variant="default">Log in</Button>
-                        <Button>Sign up</Button>
-                    </Group>
-                </Group>
-            </header>
-            {children}
-        </Box>
+                </header>
+                {children}
+            </Box>
+            <Modal opened={opened} onClose={close} title="Authentication">
+                <LoginForm />
+            </Modal>
+        </>
     );
 };
 
