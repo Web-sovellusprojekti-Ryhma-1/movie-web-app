754e34e3483b47f68e266a5962bb40a3ed0ca313
t2alni00 <t2alni00@students.oamk.fi>
2025-09-29 11:51:58 +0300

    changed user routes

--- src/api/Auth.ts
+++ src/api/Auth.ts
@@ -2,11 +2,11 @@ import { axiosPrivate } from './Axios.ts'
 import type { SignUpType, LoginType } from '../context/AuthProvider.tsx'
 
 export const SignUpRequest = async (userData: SignUpType) => {
-        const response = await axiosPrivate.post("user/signup", userData)
+        const response = await axiosPrivate.post("api/user/signup", userData)
         return response.data
 }
 
 export const LoginRequest = async (userData: LoginType) => {
-        const response = await axiosPrivate.post("user/signin", userData)
+        const response = await axiosPrivate.post("api/user/signin", userData)
         return response.data
 }
